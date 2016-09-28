34ba4b2937c3e06df98f6b0e934f698d575085e0
tonychen09 <tony_chen93@yahoo.com>
2016-09-28 14:18:04 +0700

    'nestedArray2'

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,21 +1,30 @@
 var roster = [
     ["Number", "Name", "Position", "Points per Game"],
-    ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
-    ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
-    ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-    ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
-    ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
+    [12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+    [9, "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
+    [31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+    [7, "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
+    [22, "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
 ]
 
-// [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+var list = 'Number : '+roster[1][0]+' Name : '+roster[1][1]+' Position : '+roster[1][2]+' Points per Game '+roster[1][3]
+var list2 = 'Number : '+roster[2][0]+' Name : '+roster[2][1]+' Position : '+roster[2][2]+' Points per Game '+roster[2][3]
 
+function listIndexZero(index)
+{
+  var IndexZero =  {'Number':roster[index][0],'Name':roster[index][1],'Position':roster[index][2],'Points per Game':roster[index][3]}
+  return IndexZero
+}
 function convert_roster_format(nestedArray) {
-    // your convert code here
+  var tampungRoster = []
+    for(var i=1;i<=4;i++)
+    {
+      tampungRoster.push(listIndexZero(i))
+    }
+  return tampungRoster
 }
 
-var object_roster = convert_roster_format(roster)
+ var object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
-
-// => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
-
-console.log(object_roster[0]["Name"] == "Joe Schmo") // outputs true
+// // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25]
+console.log(object_roster[0]['Name'] === 'Joe Schmo') // outputs true
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,21 @@
+function randomTictactoe()
+{
+  var tampungTicTac = []
+  for(var i=0;i<=2;i++){
+    if(Math.random() < 0.5)tampungTicTac[i] = 'X'
+    else tampungTicTac[i] = 'O'
+  }
+  return tampungTicTac
+}
+
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  // var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var tampungBoard = []
+  for(var i=0;i<=2;i++)
+  {
+    tampungBoard.push(randomTictactoe())
+  }
+  return tampungBoard
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
