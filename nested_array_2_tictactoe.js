23ca449e8a46f1a2bb6a521715cee16850310037
broerjuang <juangwiantoro@gmail.com>
2016-09-28 14:10:45 +0700

    Release 0 Tic-tac-toe revisited

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,22 @@
+// Initiate main function that generates random tic tac toe board each time it is called
+
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var table = [];
+  for (var i = 0; i < 9; i += 1) {
+    table[i] = [];
+    for (var j = 0; j < 9; j += 1) {
+      table[i][j] = tic_tac_toe_random();
+    }
+  }
+  return table;
 }
 
+// Make a function called tic tac toe random that generates random output
+// whether it "X" or "0"
+function tic_tac_toe_random() {
+  var computer_random = Math.round(Math.random());
+  return computer_random === 1 ? "X" : "0";
+}
+
+// Result
 console.log(tic_tac_toe_board()) // => make a random tic tic board
