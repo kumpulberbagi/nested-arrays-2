2e8b5808df92bffe72b84f7b5e070a024d17a9e4
Alexander Hendrawan tjahja <email": null>
2016-09-28 20:33:22 +0700

    revisi nested 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,21 +1,94 @@
 var roster = [
-    ["Number", "Name", "Position", "Points per Game"],
-    ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
-    ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
-    ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-    ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
-    ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
+  ["Number", "Name", "Position", "Points per Game"],
+  ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+  ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
+  ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+  ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
+  ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]],
+  ["20", "Ale ale man", "Power Forward ", [11, 0, 23, 17, 0]]
 ]
 
-// [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+console.log(roster.length)
 
-function convert_roster_format(nestedArray) {
-    // your convert code here
+// // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+
+function classObj(i) {
+  var obj = {
+    "Number": roster[i][0],
+    "Name": roster[i][1],
+    "Position": roster[i][2],
+    "Points per Game": roster[i][3]
+  }
+  return obj;
+}
+
+function convert_roster_format() {
+  // your convert code here
+  let pemain = [];
+  for (var i = 1; i <= roster.length - 1; i++) {
+    pemain.push(classObj(i));
+  }
+  return pemain;
 }
 
 var object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+console.log(object_roster)
+  //console.log(object_roster[2])
+  //
+  // // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
+  //
+  // console.log(object_roster[0]["Name"] == "Joe Schmo") // outputs true
+  //
+  //
+  // var Obj = {
+  //   "Number": 777,
+  //   "Name": "Alexander",
+  //   "Position": "Center",
+  //   "Points": [14, 32, 7, 0, 23]
+  // }
+  //
+  // var Obj2 = {
+  //     "Number": 12,
+  //     "Name": "Joe Schmo",
+  //     "Position": "Center",
+  //     "Points": [19, 0, 11, 22, 0]
+  //   }
+  //
+  // var array = [Obj, Obj2];
+  //
+  // console.log(array[1]["Name"]);
 
-// => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
+// var array = [
+//   [12, "alexander", "point"],
+//   [14, "good", "center"]
+// ];
+// var iMaxNum = 1;
+//
+// // Set the length of the array to iMaxNum + 1.
+// // The first array index is zero, not 1.
+// var MultiplicationTable = new Array(iMaxNum);
+//
+// // Loop for each major number (each row in the table)
+// for (var i = 0; i <= iMaxNum; i++) {
+//   // Create the columns in the table
+//   MultiplicationTable[i] = new Array();
+//
+//   // Fill the row with the results of the multiplication
+//   for (var j = 0; j <= iMaxNum + 1; j++) {
+//     MultiplicationTable[i][j] = array[i][j];
+//   }
+// }
+//
+// console.log(MultiplicationTable);
 
-console.log(object_roster[0]["Name"] == "Joe Schmo") // outputs true
+// }
+//
+// console.log(MultiplicationTable);
+
+// }
+//
+// console.log(MultiplicationTable);
+
+// }
+//
+// console.log(MultiplicationTable);
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,44 @@
-function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+function randomXO() {
+  var random = Math.random();
+  if (random < 0.5) {
+    return "O";
+  } else {
+    return "X";
+  }
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+function array2d() {
+  let tampung = [];
+  for (var j = 0; j < 3; j++) {
+    tampung = new Array(3)
+  }
+  return tampung;
+}
+
+function tic_tac_toe_board() {
+  //var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+  var tampung = [];
+
+  for (var i = 0; i < 3; i++) {
+    tampung.push(array2d());
+  }
+
+
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      tampung[i][j] = (randomXO());
+    }
+  }
+  return tampung;
+}
+
+//console.log(tic_tac_toe_board()) // => make a random tic tic board
+console.log(tic_tac_toe_board());
+
+// error karena tampung belum di bikin tempatnya
+// var tampung = [];
+// tampung[0][0].push(1);
+// console.log(tampung[0][0]);
+
+//console.log(randomXO());
