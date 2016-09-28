9a762e50ace483c658270702b18ad4eef7e7c3be
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-09-28 12:16:13 +0700

    tictactoe

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,32 @@
 function tic_tac_toe_board() {
   var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var board = []
+  for (var i=0; i<3; i++){
+    board.push([])
+  }
+  for (var i=0; i<3; i++){
+    for (var j =0; j<3; j++){
+      board[i].push(xo_random[j])
+    }
+  }
+  console.log(board);
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+
+function tic_tac_toe_random(){
+  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var board = []
+  for (var i=0; i<3; i++){
+    board.push([])
+  }
+  for (var i=0; i<3; i++){
+    for (var j =0; j<3; j++){
+      board[i].push(xo_random[Math.floor((Math.random()*8)+1)])
+    }
+  }
+  console.log(board);
+}
+
+console.log(tic_tac_toe_board())
+console.log(typeof(Math.random()));
+console.log(tic_tac_toe_random()); // => make a random tic tic board
