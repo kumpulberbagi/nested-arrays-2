5dc587713a7ddfa6d09ed637417bf8a3839bbc32
dharmadi93 <dharmadi93@gmail.com>
2016-09-28 18:52:16 +0700

    nestet array2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -9,12 +9,40 @@ var roster = [
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
+
+
+
+
 function convert_roster_format(nestedArray) {
     // your convert code here
+
+    var array = [];
+
+
+    console.log(judul);
+
+    for (var i = 1; i < nestedArray.length; i++){
+        var judul = nestedArray[0];
+        var temp = nestedArray[i];
+        // array[i] = nestedArray[i];
+        var temp = {
+            "Number": temp[0],
+            "Name": temp[1],
+            "Position": temp[2],
+            "Points per Game": temp[3]
+        }
+        array.push(temp);
+    }
+
+    return array;
+
 }
+
+
 
 var object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
+// console.log(object_roster)
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,54 @@
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+  var rand = 0, x=0,o=0, flagx=0, flago=0;
+
+  var array = [];
+
+  rand = Math.floor(Math.random() * 2) + 1;
+
+  console.log(rand);
+  if(rand == 1) {
+    for(var l=0; l<9; l++) {
+      if(flagx == 0) {
+        array.push("X");
+        // x++;
+        flagx = 1;
+        flago = 0;
+      }
+      else if(flago == 0) {
+        array.push("O");
+        // o++;
+        flagx = 0;
+        flago = 1;
+      }
+    }
+  }
+  else if(rand == 2){
+    for(var l=0; l<9; l++) {
+      if(flago == 0) {
+        array.push("O");
+        // x++;
+        flagx = 0;
+        flago = 1;
+      }
+      else if(flagx == 0) {
+        array.push("X");
+        // o++;
+        flagx = 1;
+        flago = 0;
+      }
+    }
+  }
+
+  var tictac = [];
+
+  for(var i=0; i<3; i++) {
+    tictac.push(array.slice(i, i+3));
+  }
+
+  return tictac;
+
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
