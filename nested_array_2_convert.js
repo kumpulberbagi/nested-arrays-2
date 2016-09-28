f1d6b142b2aabb4932eac0af12db06053b81b54c
elkana-ltobing <elkana.ltobing@gmail.com>
2016-09-28 20:39:04 +0700

    nested array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,21 +1,29 @@
 var roster = [
-    ["Number", "Name", "Position", "Points per Game"],
-    ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
-    ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
-    ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-    ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
-    ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
+  ["Number", "Name", "Position", "Points per Game"],
+  ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+  ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
+  ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+  ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
+  ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]],
+  ["20", "Ale ale man", "Power Forward ", [11, 0, 23, 17, 0]]
 ]
-
-// [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
-
-function convert_roster_format(nestedArray) {
-    // your convert code here
+// // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+function classObj(i) {
+  var obj = {
+    "Number": roster[i][0],
+    "Name": roster[i][1],
+    "Position": roster[i][2],
+    "Points per Game": roster[i][3]
+  }
+  return obj;
 }
-
+function convert_roster_format() {
+  // your convert code here
+  let pemain = [];
+  for (var i = 1; i <= roster.length - 1; i++) {
+    pemain.push(classObj(i));
+  }
+  return pemain;
+}
 var object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
-
-// => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
-
-console.log(object_roster[0]["Name"] == "Joe Schmo") // outputs true
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,29 @@
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  //var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+var tic_tac_toe_board = []
+
+    function kolom(){
+      var column = []
+    // var column = []
+
+    for (var i=0; i<3; i++){
+      var randomXO = Math.random();
+      if(randomXO>0.5){
+        column.push("O")
+      }
+      else if (randomXO<0.5) {
+        column.push("X")
+      }
+    }
+    return column
+    }
+
+for (var i = 0; i < 3; i++) {
+  tic_tac_toe_board.push(kolom());
 }
+
+return tic_tac_toe_board
+  }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
