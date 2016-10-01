5492aaeb0311a175dd8f74cf9b62886fd0e1b122
tonychen09 <tony_chen93@yahoo.com>
2016-10-01 12:24:21 +0700

    revisi tic-tac-toe

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -9,13 +9,32 @@ function randomTictactoe()
 }
 
 function tic_tac_toe_board() {
-  // var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var flag = true;
+  var countX = 0
+  var countO = 0
   var tampungBoard = []
   for(var i=0;i<=2;i++)
   {
     tampungBoard.push(randomTictactoe())
   }
-  return tampungBoard
+  for(var j=0;j<tampungBoard.length;j++){
+    for(var i =0;i<tampungBoard.length;i++)
+    {
+      if(tampungBoard[j][i] === 'O')
+      {
+        countO++
+      }else {
+        countX++
+      }
+    }
+  }
+
+  if(countX < 4 || countO < 4){
+    return tic_tac_toe_board()
+  }else if(countO === 4 || countO === 5 || countX === 4 || countX ===5)
+  {
+    return tampungBoard
+  }
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
