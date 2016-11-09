0d2f795d2ee69785a339e97c1c38a3fcc3888192
Your Name <name@example.com>
2016-11-09 12:40:46 +0700

    test 9 elemen

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,18 @@
+
 function tic_tac_toe_board() {
-  var xo_random = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  var array = []
+  for (var i = 1; i < 10; i++) {
+    array.push(i)
+  }
+
+  var letter_gird = [];
+  var chunk = 3;
+  for (var j = 0; j < array.length; j+=chunk) {
+    letter_gird.push(array.slice(j, j+chunk))
+  }
+
+
+  return letter_gird;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
